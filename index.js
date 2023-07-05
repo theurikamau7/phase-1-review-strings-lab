8e98c52b284e6849ff768c33d1fbb73bb8dfaa50
Iankamau1 <theurikamau7@gmail.com>
2023-07-05 17:35:57 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code in this file!
+
+let currentUser = 'Ian'
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
+
+currentUser = welcomeMessage
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+
+let excitedWelcomeMessage = upperCaseCurrentUser
+
+
+
+excitedWelcomeMessage = upperCaseCurrentUser + '!'
+
+let shortGreeting = `Welcome, ${firstInitial} ${restOfName} !`
